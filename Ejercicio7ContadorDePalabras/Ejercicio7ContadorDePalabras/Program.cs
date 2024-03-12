0b8a29fb0fef3ac05f7bf129999055ac7f5ac6a4
daavid-om <145343929+daavid-om@users.noreply.github.com>
2024-03-12 12:16:51 +0100

    Add files via upload

---
+++ Ejercicio7ContadorDePalabras/Ejercicio7ContadorDePalabras/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ejercicio7ContadorDePalabras
+{
+    /// <summary>
+    /// Clase principal de la aplicación
+    /// 120324 - dom
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Método principal de la aplicación
+        /// 120324 - dom
+        /// </summary>
+        static void Main()
+        {
+            string ej = "Esta es una cadena de ejemplo para contar palabras";
+            int contadordepalabras = CountWords(ej);
+            Console.WriteLine($"Número de palabras: {contadordepalabras}");
+        }
+
+        static int CountWords(string ej)
+        {
+            string[] palabras = ej.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return palabras.Length;
+        }
+    }
+}
